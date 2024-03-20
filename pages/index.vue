214cb8dcaed60105a99a324950d6a2166bfa716d
Igor Kordiukiewicz <igorkordiukiewicz@gmail.com>
2024-03-20 22:05:48 +0100

    Added OpenAI api key input

---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<script setup>
+const apiKeyInput = ref();
+
+function updateApiKey() {
+    localStorage.setItem('apiKey', apiKeyInput.value);
+}
+</script>
+
+<template>
+    <div>
+      <form>
+        <label>Enter OpenAI API Key</label>
+        <input type="text" v-model="apiKeyInput" />
+        <button type="submit" @click="updateApiKey()">Save</button>
+      </form>
+    </div>
+</template>
+  
