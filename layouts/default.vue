8a9558f1f4d1d905b0b4077b805c530315d36e4f
Igor Kordiukiewicz <igorkordiukiewicz@gmail.com>
2024-03-28 13:17:56 +0100

    Created chat UI

--- layouts/default.vue
+++ layouts/default.vue
@@ -15,7 +15,7 @@
 body {
     margin: 0;
     overflow: hidden;
-    /*background: linear-gradient(334deg, #28a08c, #00dc8c);*/
+    background: linear-gradient(334deg, #28a08c, #00dc8c);
     background: #141414;
     background-size: 180% 180%;
     animation: gradient-animation 6s ease infinite;
@@ -37,7 +37,7 @@ body {
 
 .main-container {
     height: 90vh;
-    width: 40%;
+    width: 60%;
     margin: 5vh auto;
     display: flex;
     flex-direction: column;
@@ -47,17 +47,17 @@ body {
 .nav-bar {
     display: flex;
     justify-content: space-around;
-    background: #212121;
+    background: #212121cc;
     padding: 0.5rem;
     border-radius: 10px;
     gap: 0.5rem;
 }
 
 .content {
-    background: #212121;
+    background: #212121cc;
     border-radius: 10px;
     height: 100%;
-    padding: 0.5rem;
+    padding: 1rem;
 }
 
 .nav-btn {
@@ -75,9 +75,13 @@ body {
     border-radius: 6px;
 }
 
+.nav-btn:hover {
+    background: #81818133;
+}
+
 .router-link-exact-active {
     transform: scale(1, 1);
-    background: #19c37d;;
+    background: #19c37d;
     color: #ffffff;
 }
 </style>
--- pages/chat.vue
+++ pages/chat.vue
@@ -1,3 +1,53 @@
 <template>
-    <h1>Chat</h1>
+    <div class="chat-container">
+        <div class="messages-container">
+            <div v-for="index in 10">
+                <label>AI</label>
+                <p class="message-content">
+                    Lorem ipsum
+                </p>    
+            </div>
+            
+        </div>
+        <form class="input-container">
+            <input type="text" class="input" placeholder="Input message" />
+            <button type="submit" class="button">Send</button>
+        </form>
+
+    </div>
 </template>
+
+<style scoped>
+.chat-container {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    height: 80vh;
+}
+
+.messages-container {
+    overflow-y: scroll;
+    flex: 1;
+    width: 100%;
+    height: 100%;
+    margin-bottom: 1rem;
+}
+
+.message-content {
+    background: #3d3d3d;
+    padding: 1rem;
+    border-radius: 5px;
+    margin-top: 0.5rem;
+}
+
+.input-container {
+    width: 100%;
+    display: flex;
+    gap: 0.5rem;
+    align-items: center;
+    border-top: 2px solid #3d3d3d;
+    padding-top: 8px;
+}
+
+
+</style>
