6d715ee9c3c7e66b18d8086a7f70f43450e85960
Igor Kordiukiewicz <igorkordiukiewicz@gmail.com>
2024-03-31 19:28:29 +0200

    Extracted NavBar component

---
+++ components/NavBar.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="nav-bar">
+        <NuxtLink to="/chat" class="nav-btn">
+            <IconText text="Chat" icon="bi-chat-fill"></IconText>
+        </NuxtLink>
+        <NuxtLink to="/images" class="nav-btn">
+            <IconText text="Images" icon="bi-image"></IconText>
+        </NuxtLink>
+        <NuxtLink to="/audio" class="nav-btn">
+            <IconText text="Audio" icon="bi-music-note"></IconText>
+        </NuxtLink>
+    </div>
+</template>
+
+<style scoped>
+
+.nav-bar {
+    display: flex;
+    justify-content: space-around;
+    background: var(--background-color);
+    padding: 0.5rem;
+    border-radius: 10px;
+    gap: 0.5rem;
+}
+
+.nav-btn {
+    position: relative;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    cursor: pointer;
+    color: #818181;
+    padding-top: 0.5rem;
+    padding-bottom: 0.5rem;
+    text-decoration: none;
+    width: 100%;
+    border-radius: 5px;
+}
+
+.nav-btn:hover {
+    background: #81818133;
+}
+
+.router-link-exact-active {
+    transform: scale(1, 1);
+    background: var(--primary-color) !important;
+    color: #ffffff;
+}
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -16,17 +16,7 @@ onMounted(() => {
 <template>
     <template v-if="hasApiKey">
         <div class="main-container">
-            <div class="nav-bar">
-                <NuxtLink to="/chat" class="nav-btn">
-                    <IconText text="Chat" icon="bi-chat-fill"></IconText>
-                </NuxtLink>
-                <NuxtLink to="/images" class="nav-btn">
-                    <IconText text="Images" icon="bi-image"></IconText>
-                </NuxtLink>
-                <NuxtLink to="/audio" class="nav-btn">
-                    <IconText text="Audio" icon="bi-music-note"></IconText>
-                </NuxtLink>
-            </div>
+            <NavBar></NavBar>
             <div class="content">
                 <slot />
             </div>
@@ -46,7 +36,6 @@ body {
     margin: 0;
     overflow: hidden;
     background: linear-gradient(334deg, #28a08c, #141414);
-    /*background: #141414;*/
     background-size: 180% 180%;
     animation: gradient-animation 6s ease infinite;
     color: #ececec;
@@ -74,46 +63,12 @@ body {
     gap: 1rem;
 }
 
-.nav-bar {
-    display: flex;
-    justify-content: space-around;
-    background: var(--background-color);
-    padding: 0.5rem;
-    border-radius: 10px;
-    gap: 0.5rem;
-}
-
 .content {
     background: var(--background-color);
     border-radius: 10px;
     height: 100%;
     padding: 1rem;
     overflow-y: hidden;
-}
-
-.nav-btn {
-    position: relative;
-    width: 12rem;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    cursor: pointer;
-    color: #818181;
-    padding-top: 0.5rem;
-    padding-bottom: 0.5rem;
-    text-decoration: none;
-    width: 100%;
-    border-radius: 6px;
-}
-
-.nav-btn:hover {
-    background: #81818133;
-}
-
-.router-link-exact-active {
-    transform: scale(1, 1);
-    background: var(--primary-color) !important;
-    color: #ffffff;
 }
 
 .settings-button {
